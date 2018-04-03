e71e454fa51fb5e07c6217eb850069b03e71ad33
Lucas Menendez <epucas@gmail.com>
2018-04-03 13:32:39 +0200

    PoS Tagging endpoint added

--- api.go
+++ api.go
@@ -9,51 +9,41 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"github.com/beeva-labs/postagger"
 )
+
+func checkInput(w http.ResponseWriter, r *http.Request) (input string) {
+	if input = r.FormValue("input"); input == "" {
+		http.Error(w, "no input text provided", 400)
+		log.Panic("No input")
+	}
+
+	return
+}
 
 func responseJson(w http.ResponseWriter, d interface{}) {
 	if res, e := json.Marshal(d); e != nil {
+		http.Error(w, "Error parsing JSON response.", 500)
 		log.Panic(e)
 	} else {
 		w.Write(res)
 	}
 }
 
-func summaryHandler(w http.ResponseWriter, r *http.Request) {
-	var e error
-	var input string
-	if input = r.FormValue("input"); input == "" {
-		log.Panic("No input")
-	}
-
-	var lang string = langdetector.Suggest(input)
-	var s *summarizer.Text
-	if s, e = summarizer.NewText(input, lang); e != nil {
-		log.Panic(e)
-	}
-
-	d := map[string]interface{} { "summary": s.Summarize() }
-	responseJson(w, d)
-}
-
 func languageHandler(w http.ResponseWriter, r *http.Request) {
-	var input string
-	if input = r.FormValue("input"); input == "" {
-		log.Panic("No input")
-	}
+	var input string = checkInput(w, r)
 
 	d := map[string]interface{} {"lang": langdetector.Suggest(input)}
 	responseJson(w, d)
 }
 
 func tokenizeHandler(w http.ResponseWriter, r *http.Request) {
-	var input string
-	if input = r.FormValue("input"); input == "" {
-		log.Panic("No input")
-	}
+	var (
+		tokens [][]string
+		input string = checkInput(w, r)
+		sentences []string = tokenizer.Sentences(input)
+	)
 
-	var tokens [][]string
-	var sentences []string = tokenizer.Sentences(input)
 	if len(sentences) > 0 {
 		for _, s := range sentences {
 			tokens = append(tokens, tokenizer.Words(s))
@@ -66,6 +56,61 @@ func tokenizeHandler(w http.ResponseWriter, r *http.Request) {
 	responseJson(w, d)
 }
 
+func postaggingHandler(w http.ResponseWriter, r *http.Request) {
+	var models string = os.Getenv("MODELS")
+	if models == "" {
+		http.Error(w, "No models configured for PoS tagging", 500)
+		log.Fatal("Empty MODELS env variable")
+	}
+
+	var (
+		input string = checkInput(w, r)
+		lang string = langdetector.Suggest(input)
+	)
+
+	var tokens []string
+	if sentences := tokenizer.Sentences(input); len(sentences) > 0 {
+		for _, s := range sentences {
+			tokens = append(tokens, tokenizer.Words(s)...)
+		}
+	} else {
+		tokens = append(tokens, tokenizer.Words(input)...)
+	}
+
+	var modelPath string = fmt.Sprintf("%s/%s", models, lang)
+	if model, e := postagger.LoadModel(modelPath); e != nil {
+		http.Error(w, "Error loading model for PoS tagging", 500)
+		log.Fatal(e)
+	} else {
+		var (
+			tagger *postagger.Tagger = postagger.NewTagger(model)
+			tagged [][]string = tagger.Tag(tokens)
+		)
+
+		if len(tagged) == 0 {
+			http.Error(w, "No tags founded.", 404)
+		}
+
+		d := map[string]interface{} { "tagged": tagged }
+		responseJson(w, d)
+	}
+}
+
+func summaryHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		input string = checkInput(w, r)
+		lang string = langdetector.Suggest(input)
+	)
+
+	if s, e := summarizer.NewText(input, lang); e != nil {
+		http.Error(w, "Error analyzing input text.", 500)
+		log.Fatal(e.Error())
+	} else {
+		d := map[string]interface{} { "summary": s.Summarize() }
+		responseJson(w, d)
+	}
+}
+
 func startApi() {
 	var port string = os.Getenv("PORT")
 	if port == "" {
@@ -74,8 +119,9 @@ func startApi() {
 		port = fmt.Sprintf(":%s", port)
 	}
 
-	http.HandleFunc("/summary", summaryHandler)
 	http.HandleFunc("/language", languageHandler)
 	http.HandleFunc("/tokenize", tokenizeHandler)
+	http.HandleFunc("/postagging", postaggingHandler)
+	http.HandleFunc("/summary", summaryHandler)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
